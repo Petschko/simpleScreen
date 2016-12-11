e1e58447253b1b842ad1d34dad95ff4804242a06
Petschko <peter-91@hotmail.de>
2016-12-11 17:46:14 +0100

    Created Java-Project

---
+++ src/org/petschko/simpleScreen/App.java
@@ -0,0 +1,17 @@
+package org.petschko.simpleScreen;
+
+/**
+ * Author: Peter Dragicevic [peter-91@hotmail.de]
+ * Authors-Website: http://petschko.org/
+ * Date: 11.12.2016
+ * Time: 17:32
+ * Update: -
+ * Version: 0.0.1
+ *
+ * Notes: App Class
+ */
+public class App {
+	public static void main(String[] args) {
+
+	}
+}
